77d5b720dedc663fa24f123cf53a43aff06f9ca2
shahbajsingh <shahbajs987@gmail.com>
2021-12-13 02:53:49 -0500

    Changes by kumar

--- src/Codebase/Controller.java
+++ src/Codebase/Controller.java
@@ -1,4 +1,65 @@
 package Codebase;
 
-public class Controller {
-}
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+    public class Controller {
+
+        @FXML
+        private JFXTextField textentry;
+
+        @FXML
+        private JFXButton r1;
+
+        @FXML
+        private JFXButton r2;
+
+        @FXML
+        private JFXButton r3;
+
+        @FXML
+        private JFXButton r4;
+
+        @FXML
+        private JFXTextArea textarea;
+
+        @FXML
+        private JFXButton submit;
+
+        @FXML
+        void handleR1(ActionEvent event) {
+
+
+        }
+
+        @FXML
+        void handleR2(ActionEvent event) {
+
+        }
+
+        @FXML
+        void handleR3(ActionEvent event) {
+
+        }
+
+        @FXML
+        void handleR4(ActionEvent event) {
+
+        }
+
+        @FXML
+        void handleSubmit(ActionEvent event) {
+
+        }
+
+    }
+
+
+
+
+
